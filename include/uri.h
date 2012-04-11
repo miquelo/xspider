902697d7b7d5bd44b90e68d039d753417d375cdd
Miquel Ferran <mikilaguna@gmail.com>
2012-04-11 21:40:18 +0200

    URI class definition

---
+++ include/uri.h
@@ -0,0 +1,35 @@
+/*
+ * This file is part of the XSpider project.
+ *
+ * Copyright (C) 2012-2013 Miquel Ferran <miquel.ferran.gonzalez@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+ 
+#ifndef URI_H
+#define URI_H
+
+/*!
+ * \file
+ * \brief Uniform Resource Identifier.
+ */
+ 
+#include <exception>
+
+namespace xspider {
+
+} // namespace xspider
+
+#endif // URI_H
